3294cf1e819909c43fffaf7c874bcbec6a3e5d35
Mrinal Gupta <mrinalgupta1097@gmail.com>
2023-02-13 15:36:43 +0530

    Creating custom users and encoding password

--- src/main/java/com/mrinalgupta/config/MySecurityConfig.java
+++ src/main/java/com/mrinalgupta/config/MySecurityConfig.java
@@ -1,10 +1,14 @@
 package com.mrinalgupta.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +16,32 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("john")
+                .password(this
+                        .passwordEncoder()
+                        .encode("mrinal"))
+                .roles("NORMAL");
+        auth.inMemoryAuthentication()
+                .withUser("adam")
+                .password(this.passwordEncoder()
+                        .encode("smith"))
+                .roles("ADMIN");
+    }
+
+    // when we set a password as plain string we have to set a password encoder
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 }
